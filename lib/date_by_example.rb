8202ff27b85a02909169370ca5132916ac453186
Noel Rappin <noelrappin@gmail.com>
2018-06-10 12:53:21 -0500

    Slight tweak

--- lib/date_by_example.rb
+++ lib/date_by_example.rb
@@ -2,10 +2,21 @@ require "date"
 require "date_by_example/version"
 require "date_by_example/example_formatter"
 require "benchmark"
+require "strscan"
 
 module DateByExample
 
   def self.benchmark
+    dt = DateTime.new(2018, 6, 9, 13, 15)
+    Benchmark.bm do |x|
+      x.report do
+        1_000_000.times do
+          ExampleFormatter.new("2 Jan 06 15:04").format(dt)
+        end
+      end
+    end
+
+    # GSUB version is 16.15 seconds
 
   end
 
